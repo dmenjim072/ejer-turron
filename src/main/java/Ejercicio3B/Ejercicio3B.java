1f2b4fae381bcb7d2b14f9adc2719769cf70e70d
david <dmenjim072@g.educaand.es>
2022-10-19 14:43:23 +0200

    ejer turron

---
+++ src/main/java/Ejercicio3B/Ejercicio3B.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package Ejercicio3B;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author david
+ */
+public class Ejercicio3B {
+
+    public static void main(String[] args) {
+        
+        // Declaramos y pedimos los datos con JOption
+        String codProducto = JOptionPane.showInputDialog("Introduce el codigo: \n"
+                + "M1 (mantecados de limon)\n"
+                + "M2 (mazapanes)\n"
+                + "P1 (polvorones)\n"
+                + "T1 (turron de chocolate)\n"
+                + "T2 (turron clasico)");        
+        
+        
+        
+        
+        
+    }
+}
